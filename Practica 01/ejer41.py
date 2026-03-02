97c2f40753d18723e80318e243c624550dbf10ff
Juan Jose Pacheco Garcia <pachecogarciasjuanjose@gmail.com>
2026-03-02 18:52:32 -0400

    Ajustes en ejer41

--- Practica 01/ejer41.py
+++ Practica 01/ejer41.py
@@ -14,4 +14,15 @@ numeros=[float(i) for i in entrada.split()]
 calc=Estadistica(numeros)
 print("el promedio es: ",calc.promedio())
 print("la desviacion estandar es: ",calc.desviacion())
-        
+
+#VENTAJAS DE UTILIZAR PROGRAMACION ORIENTADA A OBJETOS(P.O.O)
+
+# Reutilización: Puedes usar el mismo código en diferentes partes sin copiar y pegar (usando la Herencia).
+
+# Orden: El código se divide en "objetos" independientes, lo que hace que sea más fácil de leer y entender.
+
+# Fácil mantenimiento: Si algo falla, solo arreglas el objeto dañado sin romper todo el programa.
+
+# Seguridad: Proteges los datos importantes para que no cualquiera pueda modificarlos (usando el Encapsulamiento).
+
+# Flexibilidad: Un mismo comando puede hacer cosas distintas según el objeto que lo reciba.
